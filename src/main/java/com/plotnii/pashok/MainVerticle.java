cffb2cbd3c3601752f798555bb0cdd25ce416d49
kopn9k <pashaorashkov@mail.ru>
2025-01-09 15:29:35 +0300

    Add vertx request router

--- src/main/java/com/plotnii/pashok/MainVerticle.java
+++ src/main/java/com/plotnii/pashok/MainVerticle.java
@@ -1,16 +1,23 @@
 package com.plotnii.pashok;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     public static int PORT = 8080;
+    public static String NAME_PARAM = "name";
 
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(event -> {
-                    event.response().end("Hello plotnii Pashok!");
-                })
-                .listen(PORT);
+        Router router = Router.router(vertx);
+        router.route("/api/v1/greeting").handler(context -> {
+            context.request().response().end("Hello plotnii Pashok!");
+        });
+        router.route("/api/v1/greeting/:" + NAME_PARAM).handler(context -> {
+            String name = context.pathParam(NAME_PARAM);
+            context.request().response().end(String.format("Hello plotnii Pashok and %s!", name));
+        });
+        vertx.createHttpServer().requestHandler(router).listen(PORT);
     }
 }
